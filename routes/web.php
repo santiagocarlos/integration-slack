23fdc45ef45ce478cc1f801a5d145103b9a8f36c
Carlos Santiago <sscarlos56@gmail.com>
2023-12-12 17:39:50 -0400

    change method in route

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/slack', function () {
 
 Route::get('principal/{user}', [SlackController::class, 'index']);
 
-Route::get('/get-response-slack', [SlackController::class, 'verifyIfExistsDataToUser']);
+Route::get('/get-response-slack', [SlackController::class, 'getResponseSlack']);
 
 
 Route::middleware('auth')->group(function () {
